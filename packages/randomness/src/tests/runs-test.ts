5ff4370138d23d04355c8a25b709dea1a2cfbd7a
Mine Yutaka <dhj.ytk.npu@gmail.com>
2025-12-10 19:48:58 +0900

    [fix] Runs Testのロジック修正

--- packages/randomness/src/tests/runs-test.ts
+++ packages/randomness/src/tests/runs-test.ts
@@ -4,13 +4,17 @@ import { getCounts } from '../utils/counter';
 
 const test: RandomnessTest = (bits: Bit[], alpha = 0.01) => {
   const n = bits.length;
+  if (n < 100) {
+    throw new Error('Too little data for test. Supply at least 100 bits');
+  }
+
   const [, ones] = getCounts(bits);
   const proportion = ones / n;
   const tau = 2 / Math.sqrt(n);
   if (Math.abs(proportion - 0.5) > tau) return [false, 0];
 
   let observedRuns = 0;
-  for (let i = 0; i < bits.length; i++) {
+  for (let i = 0; i < n - 1; i++) {
     if (bits[i] !== bits[i + 1]) observedRuns += 1;
   }
 
